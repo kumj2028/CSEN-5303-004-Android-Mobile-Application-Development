0748b23cdfbb70c246ddcee89c3231a5b1db1d36
kumj2028 <112904420+kumj2028@users.noreply.github.com>
2023-06-02 13:11:49 -0500

    changed HelloWorld to use empty view new project

--- HelloWorld/README.md
+++
@@ -1,2 +0,0 @@
-This project was made using the "No Activity" option when choosing to create a new project.
-From there, the MainActivity.java and activity_main.xml were created following https://www.tutorialspoint.com/android/android_hello_world_example.htm
--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.helloworld;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
