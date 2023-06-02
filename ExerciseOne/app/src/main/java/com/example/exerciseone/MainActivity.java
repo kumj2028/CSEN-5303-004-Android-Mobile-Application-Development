fc824049faccb81362e08522e833dc7d82df5bfa
kumj2028 <112904420+kumj2028@users.noreply.github.com>
2023-06-02 14:02:24 -0500

    ExerciseOne finished

---
+++ ExerciseOne/app/src/main/java/com/example/exerciseone/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.exerciseone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1;
+    private EditText editText2;
+    private TextView result;
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        result = (TextView) findViewById(R.id.textView1);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("Welcome " +
+                        editText1.getText() + " from " + editText2.getText());
+            }
+        });
+    }
+}
