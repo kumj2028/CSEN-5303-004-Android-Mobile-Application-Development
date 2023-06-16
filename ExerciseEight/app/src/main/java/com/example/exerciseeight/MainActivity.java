8601ed965451694c72efe0936fb27cba3f28cd98
kumj2028 <112904420+kumj2028@users.noreply.github.com>
2023-06-16 13:39:25 -0500

    exercise8 complete

---
+++ ExerciseEight/app/src/main/java/com/example/exerciseeight/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.exerciseeight;
+
+import static java.lang.Integer.max;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1 = findViewById(R.id.textView1);
+
+        String name = "";
+        String columnNames[];
+
+        SQLiteDatabase db = openOrCreateDatabase("EmployeeDatabase", MODE_PRIVATE, null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS EmployeeInformation (LastName VARCHAR, FirstName VARCHAR, PhoneNumber VARCHAR);");
+        db.execSQL("INSERT INTO EmployeeInformation VALUES('Jones', 'Randy', '555-4325');");
+        db.execSQL("INSERT INTO EmployeeInformation VALUES('Smith', 'Robert', '555-1641');");
+        db.execSQL("INSERT INTO EmployeeInformation VALUES('Garcia', 'Roland', '555-4142');");
+
+        // following lines query database and echo results to logcat
+        Cursor cursor = db.rawQuery("SELECT * from EmployeeInformation", null);
+        columnNames = cursor.getColumnNames();
+
+        cursor.moveToFirst();
+        for (String column: columnNames) {
+            name += cursor.getString(cursor.getColumnIndexOrThrow(column)) + ", ";
+        }
+        name = name.substring(0, name.length() - 2);
+        Log.d("results", name);
+        tv1.append(name + "\n");
+
+        name = "";
+        cursor.moveToNext();
+        for (String column: columnNames) {
+            name += cursor.getString(cursor.getColumnIndexOrThrow(column)) + ", ";
+        }
+        name = name.substring(0, name.length() - 2);
+        Log.d("results", name);
+        tv1.append(name + "\n");
+
+        name = "";
+        cursor.moveToNext();
+        for (String column: columnNames) {
+            name += cursor.getString(cursor.getColumnIndexOrThrow(column)) + ", ";
+        }
+        name = name.substring(0, name.length() - 2);
+        Log.d("results", name);
+        tv1.append(name + "\n");
+
+        db.close();
+    }
+}
